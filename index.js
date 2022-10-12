a9e1c5e33be3c0ed2818b7b8b97e5cf83f663157
Your Name <you@example.com>
2022-10-12 16:13:28 -0500

    evidencia

--- index.js
+++ index.js
@@ -20,17 +20,47 @@ console.log(`apellido: ${apellido}`.bgMagenta)
 console.log(`altura: ${altura}`.bgMagenta)*/
 
 const materias = require('./materias')
+//crear materia y ponerla en arreglo de materias
+//push()
+materias.push({
+    nombre: "Java",
+    instructor: "Jorge Celedon",
+    notas: [
+        3.4, 4.0
+    ],
+    tipo:"Tecnica"
+
+})
 //recorrido de arreglo
 materias.forEach((materia)=>{
-    if(materia.instructor === 'Cristian Buitrago'){
-        console.log(`nombre:${materia.instructor}`.bgMagenta)
-        console.log(`nombre:${materia.notas}`.bgBlue)
-    }
+   
+        console.log(`nombre:${materia.nombre}`.bgMagenta)
+        console.log(`instructor:${materia.instructor}`.bgMagenta)
+        console.log(`nota:${materia.notas}`.bgMagenta)
+        console.log('----------')
+    
 })
+//eliminar materia: splice
 
+
+
+
+//actualizar 
+//1. encontrar elemento a actualizar: findIndex
+let indice = materias.findIndex((materia)=> materia.instructor ==="Luis Baquero")
+console.log(indice)
+materias[indice].instructor = "Tatiana Cabrer"
+
+let calificacion = materias.findIndex((materia)=> materia.notas === 4.2) 
+console.log(calificacion)
+materias[indice].notas = 3.5
+console.log(materias)
+
+materias.splice(0,2)
+console.log(materias)
 //map: metodo de arreglos es6
 //crear un arreglo apartir de otro
-const profesores = materias.map((materia)=>{
+/*const profesores = materias.map((materia)=>
     return materia.instructor
 })
 
@@ -41,4 +71,7 @@ console.log(profesores)
 const PHP = materias.filter((materia)=>{
     return materia.instructor === "Cristian Buitrago"
 })
-console.log(PHP)
+
+//crear nueva materia:
+
+console.log(PHP)*/
